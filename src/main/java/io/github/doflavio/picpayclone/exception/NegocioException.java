f2bdacbe2c65dec53a1e366416250eef2be17e5d
doflavio <doflavio@gmail.com>
2021-06-07 10:31:57 -0300

    Criação de exception e Handler para a exception

---
+++ src/main/java/io/github/doflavio/picpayclone/exception/NegocioException.java
@@ -0,0 +1,16 @@
+package io.github.doflavio.picpayclone.exception;
+
+public class NegocioException extends RuntimeException {
+
+	private static final long serialVersionUID = -7507642233804283142L;
+
+	public NegocioException(String mensagem) {
+		super(mensagem);
+	}
+
+	public NegocioException(String mensagem, Exception exception) {
+		super(mensagem, exception);
+	}
+
+	
+}
