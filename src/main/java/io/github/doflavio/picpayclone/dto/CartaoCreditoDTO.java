a0045642c18db3c45c05fed478d2c4f12a6bd064
doflavio <doflavio@gmail.com>
2021-06-07 08:45:14 -0300

    Criação e configuração dos DTOs

---
+++ src/main/java/io/github/doflavio/picpayclone/dto/CartaoCreditoDTO.java
@@ -0,0 +1,37 @@
+package io.github.doflavio.picpayclone.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.sun.istack.NotNull;
+
+import io.github.doflavio.picpayclone.enums.BandeiraCartao;
+import lombok.Data;
+
+@Data
+@JsonInclude(Include.NON_NULL)
+public class CartaoCreditoDTO {
+
+	@NotBlank
+	private BandeiraCartao bandeira;
+
+	@NotBlank
+	private String codigoSeguranca;
+
+	@NotBlank
+	private String dataExpiracao;
+
+	@NotBlank
+	private String nomeTitular;
+	
+	private String numero;
+
+	private String numeroToken;
+
+	@NotNull
+	private UsuarioDTO usuario;
+
+	private Boolean isSalva = false;
+
+}
--- src/main/java/io/github/doflavio/picpayclone/modelo/teste.java
+++
@@ -1,5 +0,0 @@
-package io.github.doflavio.picpayclone.modelo;
-
-public class teste {
-
-}
